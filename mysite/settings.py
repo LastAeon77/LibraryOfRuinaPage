8cfeb38f520d863b7b5636d36bf0b1b383374377
LastAeon77 <kswasdikulavath23@my.whitworth.edu>
2020-11-13 04:37:32 -0800

    deleted backup

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-with open("resources/settings.json", "r") as f:
-    key = json.load(f)
+# with open("resources/settings.json", "r") as f:
+#     key = json.load(f)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = key["django"]["Secret_key"]
