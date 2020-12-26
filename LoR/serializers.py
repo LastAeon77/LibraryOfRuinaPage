4d88573d77efa4f5258cc0da33e18c78720c14ed
Kamontat Swasdikulavath <kswasdikulavath23@my.whitworth.edu>
2020-12-25 22:42:08 -0800

    I'm sinking too deep into this rabbit serial hole

--- LoR/serializers.py
+++ LoR/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
-from LoR.models import Deck, Card, Rank
+from LoR.models import Deck, Card, Rank, RelDeck
+
+
+class CardCountSerializers(serializers.ModelSerializer):
+    card_id = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = RelDeck
+        fields = ["card_count", "card_id"]
 
 
 class CardDeckSerializers(serializers.ModelSerializer):
@@ -9,6 +17,7 @@ class CardDeckSerializers(serializers.ModelSerializer):
 
 
 class DeckSerializers(serializers.ModelSerializer):
+    card_count = serializers.SerializerMethodField("get_card_set")
     cards = CardDeckSerializers(many=True, read_only=True)
     effect = serializers.StringRelatedField(many=True, read_only=True)
     Recc_Floor = serializers.StringRelatedField(read_only=True)
@@ -19,7 +28,11 @@ class DeckSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = Deck
-        fields = ("__all__")
+        fields = "__all__"
+
+    def get_card_set(self, instance):
+        reldecks = instance.reldeck_set.all().order_by("card_id")
+        return CardCountSerializers(reldecks, many=True).data
 
 
 class CardSerializers(serializers.ModelSerializer):
