4c9b4f5fe07b91583d7f1112d6c8d5d3c5f95a9c
Kamontat Swasdikulavath <kswasdikulavath23@my.whitworth.edu>
2020-12-25 21:25:28 -0800

    made serializing EVEN better

--- LoR/serializers.py
+++ LoR/serializers.py
@@ -2,17 +2,24 @@ from rest_framework import serializers
 from LoR.models import Deck, Card, Rank
 
 
+class CardDeckSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ["Name", "ImgPath"]
+
+
 class DeckSerializers(serializers.ModelSerializer):
-    cards = serializers.StringRelatedField(many=True, read_only=True)
+    cards = CardDeckSerializers(many=True, read_only=True)
     effect = serializers.StringRelatedField(many=True, read_only=True)
     Recc_Floor = serializers.StringRelatedField(read_only=True)
     Recc_Page = serializers.StringRelatedField(read_only=True)
     Recc_Rank = serializers.StringRelatedField(read_only=True)
     creator = serializers.StringRelatedField(read_only=True)
+    cardImage = serializers.SerializerMethodField
 
     class Meta:
         model = Deck
-        fields = "__all__"
+        fields = ("__all__")
 
 
 class CardSerializers(serializers.ModelSerializer):
