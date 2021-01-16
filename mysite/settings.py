13afe1c964a22ee1ff2abdd5009b015c9bea3516
Kamontat <siris.mark@gmail.com>
2021-01-15 17:16:27 -0800

    updated settings for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,27 +151,31 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+
 ##################################
 
-AWS_ACCESS_KEY_ID = f"{os.environ.get('S3AccessID')}"
-AWS_SECRET_ACCESS_KEY = f"{os.environ.get('S3SecretKey')}"
-AWS_STORAGE_BUCKET_NAME = f"{os.environ.get('BucketName')}"
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-AWS_DEFAULT_ACL = None
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = f"{os.environ.get('S3AccessID')}"
+# AWS_SECRET_ACCESS_KEY = f"{os.environ.get('S3SecretKey')}"
+# AWS_STORAGE_BUCKET_NAME = f"{os.environ.get('BucketName')}"
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# AWS_LOCATION = 'static'
+# AWS_DEFAULT_ACL = None
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 ##################################
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATIC_URL = "/static/"
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 LOGIN_REDIRECT_URL = "HomePage"
 
@@ -181,10 +185,10 @@ LOGIN_URL = "login"
 import django_heroku
 
 django_heroku.settings(locals(), staticfiles=False)
-# STATICFILES_STORAGE = 'mysite.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'mysite.storage.WhiteNoiseStaticFilesStorage'
 
 
-# COMPRESS_ENABLED = os.environ.get("COMPRESS_ENABLED", False)
+COMPRESS_ENABLED = os.environ.get("COMPRESS_ENABLED", False)
 
 
 LOGGING = {
