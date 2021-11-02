9d26650ec61af3746dc63096c9e83236cc076c27
Kamontat <siris.mark@gmail.com>
2021-11-01 20:16:57 -0700

    updated serializer

--- LoR/serializers.py
+++ LoR/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from LoR.models import Deck, Card, Rank, RelDeck, AbnoCards, Effects
+from django.utils.translation import ngettext_lazy as unlaz
 
 
 class CardCountSerializers(serializers.ModelSerializer):
@@ -37,10 +38,39 @@ class DeckSerializers(serializers.ModelSerializer):
 class CardSerializers(serializers.ModelSerializer):
     office = serializers.StringRelatedField(read_only=True)
     rank = serializers.ReadOnlyField(source='office.Rank.Name')
+    Rarity = serializers.SerializerMethodField()
+    Type1 = serializers.SerializerMethodField()
+    Type2 = serializers.SerializerMethodField()
+    Type3 = serializers.SerializerMethodField()
+    Type4 = serializers.SerializerMethodField()
+    Type5 = serializers.SerializerMethodField()
+    CardType = serializers.SerializerMethodField()
     class Meta:
         model = Card
-        fields = "__all__"
+        fields = ('office','rank','Name',
+        'Obtainable','Cost','On_Play_Effect',
+        'Dice_Number','ImgPath','Roll1','Rarity',
+        'Eff1','Type1','CardType',
+        'Roll2','Eff2','Type2',
+        'Roll3','Eff3','Type3',
+        'Roll4','Eff4','Type4',
+        'Roll5','Eff5','Type5',
+        'slug')
 
+    def get_Rarity(self,obj):
+        return obj.get_Rarity_display()
+    def get_Type1(self,obj):
+        return obj.get_Type1_display()
+    def get_Type2(self,obj):
+        return obj.get_Type2_display()
+    def get_Type3(self,obj):
+        return obj.get_Type3_display()
+    def get_Type4(self,obj):
+        return obj.get_Type4_display()
+    def get_Type5(self,obj):
+        return obj.get_Type5_display()
+    def get_CardType(self,obj):
+        return obj.get_CardType_display()
 
 class RankSerializers(serializers.ModelSerializer):
     class Meta:
